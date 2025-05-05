3dcb763c85e5b244071614bf8b14ba06c165cc03
Abdulla Vora <143266743+AbdullaVora@users.noreply.github.com>
2025-05-06 01:54:28 +0530

    Update Preloader.jsx

--- src/components/others/Preloader.jsx
+++ src/components/others/Preloader.jsx
@@ -2,31 +2,36 @@ import React from 'react';
 import logo2 from "../../assets/logo2.png";
 
 const Preloader = () => {
-   return (
+  return (
   <>
-    <div className="loader-container">
+    <div className="fullscreen-bg">
       <img src={logo2} alt="loader" className="loader-image" />
       <span className="loader"></span>
     </div>
 
     <style>
       {`
-        .loader-container {
+        .fullscreen-bg {
           position: absolute;
-          inset: 0;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          background-color: #020B1C;
           display: flex;
           flex-direction: column;
           justify-content: center;
           align-items: center;
-          backdrop-filter: blur(12px); /* Matches backdrop-blur-md */
+          backdrop-filter: blur(12px);
           z-index: 20;
         }
 
         .loader-image {
-          margin-inline-end: 1.75rem; /* Matches me-7 */
+          margin-inline-end: 1.75rem;
           z-index: 30;
           width: 300px;
         }
+
       `}
     </style>
   </>
