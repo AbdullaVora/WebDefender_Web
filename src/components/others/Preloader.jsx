a30363c776e926624ba5196f589e4028ee02b535
Abdulla Vora <143266743+AbdullaVora@users.noreply.github.com>
2025-05-06 01:41:56 +0530

    Update Preloader.jsx

--- src/components/others/Preloader.jsx
+++ src/components/others/Preloader.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import logo2 from "../../assets/logo2.png";
 
 const Preloader = () => {
     return (
-        <>
-            <div className="se-pre-con bg-black w-100 h-100"></div>
-        </>
+        <div className="absolute inset-0 flex flex-col justify-center items-center backdrop-blur-md z-20">
+            <img src={logo2} alt="loader" width={300} className="me-7 z-30" />
+            <span className="loader z-30"></span>
+        </div>
     );
 };
 
